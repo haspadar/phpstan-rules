11573bc26871d7d5f415f63de5e72847ebb90018
Kanstantsin Mesnik <haspadar@gmail.com>
2026-03-20 10:06:28 +0300

    Set phpunit memory limit to 512M to prevent OOM on large test suite

--- .piqule.php
+++ .piqule.php
@@ -8,4 +8,5 @@ use Haspadar\Piqule\Config\OverrideConfig;
 return new OverrideConfig(new DefaultConfig(), [
     'ci.pr.max_lines_changed' => 1000,
     'typos.exclude' => ['docs/vendors/'],
+    'phpunit.php_options' => '-d memory_limit=512M',
 ]);
