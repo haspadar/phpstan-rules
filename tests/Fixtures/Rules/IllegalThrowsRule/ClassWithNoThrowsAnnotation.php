d93ba7eefaabed5b4ed01bcc519445b447ca913d
Kanstantsin Mesnik <haspadar@gmail.com>
2026-03-27 07:29:46 +0300

    Cover null doc comment branch to satisfy codecov patch requirement

--- tests/Fixtures/Rules/IllegalThrowsRule/ClassWithNoThrowsAnnotation.php
+++ tests/Fixtures/Rules/IllegalThrowsRule/ClassWithNoThrowsAnnotation.php
@@ -6,10 +6,5 @@ namespace Haspadar\PHPStanRules\Tests\Fixtures\Rules\IllegalThrowsRule;
 
 final class ClassWithNoThrowsAnnotation
 {
-    /**
-     * @param int $value some value
-     *
-     * @return void
-     */
     public function run(int $value): void {}
 }
